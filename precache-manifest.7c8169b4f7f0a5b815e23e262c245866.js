d6f3aa529b699e9a795fdf9e31f270cc45915dbe
katmdev <katherinedevrieze@gmail.com>
2020-06-24 23:07:23 -0400

    Updates

---
+++ precache-manifest.7c8169b4f7f0a5b815e23e262c245866.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85538bc1b5fe7e98f1988416d6698d32",
+    "url": "/katDevriezeProject5/index.html"
+  },
+  {
+    "revision": "9053dd88d56ccd2246cb",
+    "url": "/katDevriezeProject5/static/css/main.c99a588d.chunk.css"
+  },
+  {
+    "revision": "102db1ee95e59ae6e6fa",
+    "url": "/katDevriezeProject5/static/js/2.efcab77f.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/katDevriezeProject5/static/js/2.efcab77f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9053dd88d56ccd2246cb",
+    "url": "/katDevriezeProject5/static/js/main.76ffbef9.chunk.js"
+  },
+  {
+    "revision": "0ea3c4c2ecc6ba749a37",
+    "url": "/katDevriezeProject5/static/js/runtime-main.26e8e00f.js"
+  }
+]);
